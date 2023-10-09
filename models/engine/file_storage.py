171339652d2567ff1a91ae483edec693deeed492
louzanizineddine <louzanizineddine@gmail.com>
2023-10-09 14:54:27 +0200

    start of task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import json
+
+class FileStorage:
+    __file_path = ''
+    __objects = {}
+
+    def all(self):
+        #returns dic of __objects
+        return self.__objects
+    
+    def new(self, obj):
+        #sets in __objects the obj with key <obj class name>.id
+        self.__objects["BaseModel." + obj.id]
+
+    def save(self):
+        #serializes __objects to the JSON file (path: __file_path)
+        json.dump(self.__objects, self.__file_path)
+
+    def reload(self):
+        #  deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
+        #  otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+        __object = json.load(self.__file_path)
