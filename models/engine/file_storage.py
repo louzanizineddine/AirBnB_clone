5bf24a48c6e00de0f7f18b268b86faaa9ab0ebfd
26fati <fatibeautiful1@gmail.com>
2023-10-14 13:15:34 +0100

    task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,80 +46,78 @@ class FileStorage:
             pass
         else:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-                obj_dict = json.load(f)
+                dic = json.load(f)
 
-                for k, v in obj_dict.items():
+                for k, v in dic.items():
                     class_name = v["__class__"]
                     match class_name:
                         case 'User':
                             # create a new User
-                            obj_dict[k] = User(**v)
+                            dic[k] = User(**v)
                             # check if the key is in the dictionary
                             # if it is, update the value
                             # if not, create a new key-value pair
 
                             if "first_name" in v:
-                                obj_dict[k].firs_name = v["first_name"]
+                                dic[k].firs_name = v["first_name"]
                             if "last_name" in v:
-                                obj_dict[k].last_name = v["last_name"]
+                                dic[k].last_name = v["last_name"]
                             if "email" in v:
-                                obj_dict[k].email = v["email"]
+                                dic[k].email = v["email"]
                             if "password" in v:
-                                obj_dict[k].password = v["password"]
+                                dic[k].password = v["password"]
 
                         case 'BaseModel':
-                            obj_dict[k] = BaseModel(**v)
+                            dic[k] = BaseModel(**v)
 
                         case 'Place':
-                            obj_dict[k] = Place(**v)
+                            dic[k] = Place(**v)
                             if "name" in v:
-                                obj_dict[k].name = v["name"]
+                                dic[k].name = v["name"]
                             if "city_id" in v:
-                                obj_dict[k].city_id = v["city_id"]
+                                dic[k].city_id = v["city_id"]
                             if "user_id" in v:
-                                obj_dict[k].user_id = v["user_id"]
+                                dic[k].user_id = v["user_id"]
                             if "description" in v:
-                                obj_dict[k].description = v["description"]
+                                dic[k].description = v["description"]
                             if "number_rooms" in v:
-                                obj_dict[k].number_rooms = v["number_rooms"]
+                                dic[k].number_rooms = v["number_rooms"]
                             if "number_bathrooms" in v:
-                                obj_dict[k].number_bathrooms
-                                = v["number_bathrooms"]
+                                dic[k].number_bathrooms = v["number_bathrooms"]
                             if "max_guest" in v:
-                                obj_dict[k].max_guest = v["max_guest"]
+                                dic[k].max_guest = v["max_guest"]
                             if "price_by_night" in v:
-                                obj_dict[k].price_by_night
-                                = v["price_by_night"]
+                                dic[k].price_by_night = v["price_by_night"]
                             if "latitude" in v:
-                                obj_dict[k].latitude = v["latitude"]
+                                dic[k].latitude = v["latitude"]
                             if "longitude" in v:
-                                obj_dict[k].longitude = v["longitude"]
+                                dic[k].longitude = v["longitude"]
 
                         case 'State':
-                            obj_dict[k] = State(**v)
+                            dic[k] = State(**v)
                             if "name" in v:
-                                obj_dict[k].name = v["name"]
+                                dic[k].name = v["name"]
 
                         case 'Review':
-                            obj_dict[k] = Review(**v)
+                            dic[k] = Review(**v)
                             if "place_id" in v:
-                                obj_dict[k].place_id = v["place_id"]
+                                dic[k].place_id = v["place_id"]
                             if "user_id" in v:
-                                obj_dict[k].user_id = v["user_id"]
+                                dic[k].user_id = v["user_id"]
                             if "text" in v:
-                                obj_dict[k].text = v["text"]
+                                dic[k].text = v["text"]
 
                         case 'City':
-                            obj_dict[k] = City(**v)
+                            dic[k] = City(**v)
                             if "name" in v:
-                                obj_dict[k].name = v["name"]
+                                dic[k].name = v["name"]
                             if "state_id" in v:
-                                obj_dict[k].state_id = v["state_id"]
+                                dic[k].state_id = v["state_id"]
 
                         case 'Amenity':
-                            obj_dict[k] = City(**v)
+                            dic[k] = City(**v)
 
                             if "name" in v:
-                                obj_dict[k].name = v["name"]
+                                dic[k].name = v["name"]
 
-                FileStorage.__objects = obj_dict
+                FileStorage.__objects = dic
